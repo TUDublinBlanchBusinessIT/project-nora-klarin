0fe2b29f1906498ee59db0e9f9ad17b6ab9c8463
Nora Klarin <B00177535@mytudublin.ie>
2024-12-10 19:07:36 +0000

    login and register added

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import AddBookScreen from './AddBookScreen';
 import BookListScreen from './BookListScreen';
 import HomeScreen from './HomeScreen';
 import EditBookScreen from './EditBookScreen';
+import RegisterScreen from './RegisterScreen';
+import LoginScreen from './LoginScreen';
 
 
 const Stack = createStackNavigator();
@@ -12,11 +14,13 @@ const Stack = createStackNavigator();
 export default function App(){
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="LoginScreen">        
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Add Book" component={AddBookScreen} />
         <Stack.Screen name="Book List" component={BookListScreen} />
         <Stack.Screen name="EditBookScreen" component={EditBookScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
